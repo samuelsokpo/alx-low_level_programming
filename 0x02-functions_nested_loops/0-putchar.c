46762390e9a2756918780ced5a874dd2237f6d94
samuelsokpo <samuel.sunny7@icloud.com>
2022-09-13 21:10:24 -0700

    print a word and keep a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string "_putchar" from a character array.
+ *
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
